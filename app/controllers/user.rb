91c6c54811ec96877fb862652a8f28f88755e943
Marty McLaughlin <martymclaugh@gmail.com>
2016-07-18 22:08:24 -0700

    change buttons, change feed to display posts from users that the user is following

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,9 @@
 get '/profile' do
 	@user = User.find(session[:user_id])
+	@feed = []
+	@user.following.each {|user| user.posts.each{|post|  @feed << post}}
+	@user.posts.each {|post| @feed << post}
+	@feed_posts = @feed.sort_by { |obj| obj.created_at }
   if logged_in?
   	erb :'users/profile'
   else
